ccb22b7f1df15eb6e2e8a67aaecf73fd44c6fa14
AdrienGeoorge <adrien-george@outlook.fr>
2022-03-19 19:58:40 +0100

    Correction bug transitions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,32 @@
 <template>
-  <div id="home-preview-boxes">
-    <div class="boxes">
-      <div class="box-style box-visual-universe">
-        <div class="box-title">Mon univers visuel</div>
-        <div class="box-content">
-          <img src="@/assets/images/logo-box-visual-universe.svg" alt="Mon univers visuel" class="logo-boxes">
-          <a href="" class="box-button">portfolio</a>
+  <div>
+    <div id="home-preview-boxes">
+      <div class="boxes">
+        <div class="box-style box-visual-universe">
+          <div class="box-title">Mon univers visuel</div>
+          <div class="box-content">
+            <img src="@/assets/images/logo-box-visual-universe.svg" alt="Mon univers visuel" class="logo-boxes">
+            <a href="" class="box-button">portfolio</a>
+          </div>
         </div>
+        <div class="box-offbeat-visual-universe"></div>
       </div>
-      <div class="box-offbeat-visual-universe"></div>
-    </div>
 
-    <div class="boxes">
-      <div class="box-style box-experiences">
-        <div class="box-title">Mon expérience</div>
-        <div class="box-content">
-          <img src="@/assets/images/logo-box-experiences.svg" alt="Mon expérience" class="logo-boxes">
+      <div class="boxes">
+        <div class="box-style box-experiences">
+          <div class="box-title">Mon expérience</div>
+          <div class="box-content">
+            <img src="@/assets/images/logo-box-experiences.svg" alt="Mon expérience" class="logo-boxes">
 
-          <a href="" class="box-button">cv</a>
+            <a href="" class="box-button">cv</a>
+          </div>
         </div>
+        <div class="box-offbeat-experiences"></div>
       </div>
-      <div class="box-offbeat-experiences"></div>
     </div>
+    <img src="@/assets/images/waves.svg" alt="Waves" id="waves">
+    <div id="banner">Retrouvez mes derniers travaux par ici !</div>
   </div>
-  <img src="@/assets/images/waves.svg" alt="Waves" id="waves">
-  <div id="banner">Retrouvez mes derniers travaux par ici !</div>
 </template>
 
 <style src="@/assets/home.scss" lang="scss"></style>
-
-<script>
-export default {
-  name: 'Home'
-}
-</script>
