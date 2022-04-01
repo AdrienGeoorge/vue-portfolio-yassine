e89bf085581f0faf4ff9cca4477e82592636e494
AdrienGeoorge <adrien-george@outlook.fr>
2022-04-01 21:50:54 +0200

    Page expérience visuelle

---
+++ src/components/RecentProjetComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <a :href="linkUrl" target="_blank" class="project-item">
+    <img :src="hover ? red : base"
+         @mouseover="hover = true"
+         @mouseleave="hover = false"
+         alt="Mon projet" :class="[right && rightClass, projectLogoClass]">
+    <div :class="[right && rightClass, projectTextsClass]">
+      <div class="work-title">
+        <slot name="title"></slot>
+      </div>
+      <div class="work-subtitle">
+        <slot name="description"></slot>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: ['linkUrl', 'base', 'red', 'right'],
+  data () {
+    return {
+      hover: false,
+      rightClass: 'right',
+      projectTextsClass: 'project-texts',
+      projectLogoClass: 'project-logo'
+    }
+  }
+}
+</script>
--- src/views/VisualExperience.vue
+++ src/views/VisualExperience.vue
@@ -1,3 +1,53 @@
 <template>
-  <h1>Page expérience visuelle</h1>
+  <div>
+    <div id="banner">Retrouvez mes derniers travaux par ici !</div>
+    <div id="recent-projects">
+      <RecentProjetComponent
+        link-url="https://www.behance.net/gallery/136317725/Habbox"
+        :base="require('@/assets/images/works/habbox.svg')"
+        :red="require('@/assets/images/works/habbox-red.svg')"
+        :right="false"
+      >
+        <template #title>Habbox</template>
+        <template #description>La charte graphique d'un jeu vidéo</template>
+      </RecentProjetComponent>
+      <RecentProjetComponent
+        link-url="https://www.behance.net/gallery/140028441/Ubiquit"
+        :base="require('@/assets/images/works/ubiquite.svg')"
+        :red="require('@/assets/images/works/ubiquite-red.svg')"
+        :right="true"
+      >
+        <template #title>Ubiquité</template>
+        <template #description>Une série d’illustrations graphique</template>
+      </RecentProjetComponent>
+      <RecentProjetComponent
+        link-url="https://www.behance.net/gallery/116211983/Charte-Graphique-Habbo-Museum"
+        :base="require('@/assets/images/works/habbo-museum.svg')"
+        :red="require('@/assets/images/works/habbo-museum-red.svg')"
+        :right="false"
+      >
+        <template #title>Habbo Museum</template>
+        <template #description>La charte graphique d'une page communautaire</template>
+      </RecentProjetComponent>
+      <RecentProjetComponent
+        link-url="https://www.behance.net/gallery/140028589/Affiche"
+        :base="require('@/assets/images/works/affiches.svg')"
+        :red="require('@/assets/images/works/affiches-red.svg')"
+        :right="true"
+      >
+        <template #title>Affiches</template>
+        <template #description>Une série d'illustration graphique</template>
+      </RecentProjetComponent>
+    </div>
+  </div>
 </template>
+
+<style src="@/assets/visual-experience.scss" lang="scss"></style>
+
+<script>
+import RecentProjetComponent from '@/components/RecentProjetComponent'
+
+export default {
+  components: { RecentProjetComponent }
+}
+</script>
